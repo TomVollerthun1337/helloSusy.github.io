d08bd5664cda892fe0ff15702a972c7df5af7efa
Benedikt Stemmildt <benedikt.stemmildt@otto.de>
2014-08-22 11:56:55 +0200

    rahmen aufgabe

---
+++ 14/config.rb
@@ -0,0 +1,23 @@
+require 'compass/import-once/activate'
+# Require any additional compass plugins here.
+require 'susy'
+require 'breakpoint'
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+sass_dir = "/"
+css_dir = "/"
+images_dir = "/"
+fonts_dir = "/"
+javascripts_dir = "/"
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+output_style = :expanded
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# DO NOT CHANGE THIS CONFIGURATION
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = false
